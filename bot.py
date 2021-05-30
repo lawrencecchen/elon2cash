9cc6380d2cfa4af8a3716f8cd66ed89e0ad59368
lawrencecchen <md.lawrence.chen@gmail.com>
2021-05-29 22:01:23 -0700

    add dotenv

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
-# client id: 545188c06166f3d1b1107127871ca61c
-# secret: f012f21654a549ab286466fb28fb276e3c4c1021
-# https://github.com/alpacahq/alpaca-trade-api-python/
-
 import discord
 import alpaca_trade_api as trade_api
+from dotenv import load_dotenv
+import os
 
-API_KEY = "545188c06166f3d1b1107127871ca61c"
-API_SECRET = "f012f21654a549ab286466fb28fb276e3c4c1021"
-APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
+load_dotenv()
 
-api = trade_api.REST(API_KEY, API_SECRET, APCA_API_BASE_URL, 'v2')
+ALPACA_API_KEY = os.environ.get('ALPACA_API_KEY')
+ALPACA_API_SECRET = os.environ.get('ALPACA_API_SECRET')
+APCA_API_BASE_URL = os.environ.get('APCA_API_BASE_URL')
+DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
+
+
+api = trade_api.REST(ALPACA_API_KEY, ALPACA_API_SECRET, APCA_API_BASE_URL, 'v2')
 
 client = discord.Client()
 
@@ -26,4 +28,4 @@ async def on_message(message):
         clock = api.get_clock()
         await message.channel.send('The market is {}'.format('open.' if clock.is_open else 'closed.'))
 
-client.run('ODQ4MDU4MDM5NjU5MjY2MDc4.YLHFmA.-PgPrPZlZgbhGukDR6hlse-0mFQ')
+client.run(DISCORD_TOKEN)
